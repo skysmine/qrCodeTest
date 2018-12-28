176db051015284afd49b773d35502c84bde313a4
skysmine <13290053112@163.com>
2018-12-28 15:06:46 +0800

    qrCode Display

--- qrCodeTest/Form1.cs
+++ qrCodeTest/Form1.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using QRCoder;
 
 namespace qrCodeTest
 {
@@ -14,6 +16,40 @@ namespace qrCodeTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            int sourceWidth = qrCodeImage.Width;
+            int sourceHeight = qrCodeImage.Height;
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            //计算宽度的缩放比例
+            nPercentW = ((float)pictureBox1.Width / (float)sourceWidth);
+            //计算高度的缩放比例
+            nPercentH = ((float)pictureBox1.Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+            //期望的宽度
+            int destWidth = (int)(sourceWidth * nPercent);
+            //期望的高度
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            //绘制图像
+            g.DrawImage(qrCodeImage, 0, 0, destWidth, destHeight);
+            g.Dispose();
+
+            this.pictureBox1.Image = b;
         }
     }
 }
